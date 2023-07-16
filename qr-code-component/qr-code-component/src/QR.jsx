4d751bcfe8b9ee2b2e87f16feab275f606be27c4
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2023-07-16 13:05:54 -0400

    Added QR code component starter files

---
+++ qr-code-component/qr-code-component/src/QR.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+const QR = () => {
+  return (
+    <>
+      <body>
+        Improve your front-end skills by building projects Scan the QR code to
+        visit Frontend Mentor and take your coding skills to the next level
+        <div className="attribution">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://github.com/CelesTech03"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Celeste Ureña
+          </a>
+          .
+        </div>
+      </body>
+    </>
+  );
+};
+
+export default QR;
